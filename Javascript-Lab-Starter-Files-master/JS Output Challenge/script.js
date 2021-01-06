7665b01a52bd80f823990b77614f0d1505f837d6
afusco059 <70164839+afusco059@users.noreply.github.com>
2021-01-06 11:00:12 -0500

    Add files via upload

---
+++ Javascript-Lab-Starter-Files-master/JS Output Challenge/script.js
@@ -0,0 +1,36 @@
+// Enter your code below to edit index.html
+
+yourName = "Anthony Fusco"
+document.getElementById("yourName").innerHTML = yourName;
+title = "My Bio"
+document.getElementById("title").innerHTML = title;
+
+myBio = "I Like to play soccer as well as code, I am currently a Junior in High school, I also like the Browns the football team"
+document.getElementById("myBio").innerHTML = myBio;
+
+collegeChoice = "Mount Union"
+document.getElementById("collegeChoice").innerHTML = collegeChoice;
+
+programChoice = "Software Engineering"
+document.getElementById("programChoice").innerHTML = programChoice;
+
+collegeReason = "I would like to pay soccer plus it is close to my house and it has a software engineering program"
+document.getElementById("collegeReason").innerHTML = collegeReason;
+
+careerChoice = "Software Engineer"
+document.getElementById("careerChoice").innerHTML = careerChoice;
+
+companyChoice = "I would like to work at Amazon"
+document.getElementById("companyChoice").innerHTML = companyChoice;
+
+careerReason = "They have good benefits plus they have many branches all over and they offer apprenticeships"
+document.getElementById("careerReason").innerHTML = careerReason;
+
+answerOne = "It is a cross-platform interface that treats or XMl as a tree structure to represent a document"
+document.getElementById("answerOne").innerHTML = answerOne;
+
+answerTwo = "Writing into Html Element, writing into an alert box, writing into the browser console"
+document.getElementById("answerTwo").innerHTML = answerTwo;
+
+answerThree = "You can change or manipulate all Html elements, and Css"
+document.getElementById("answerThree").innerHTML = answerThree;
